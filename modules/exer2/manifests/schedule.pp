1f73287456e3f185af14eb95d38c8d3829effb96
Loven <loven.francis+genesys@gmail.com>
2017-06-20 00:07:37 +0800

    typo. replaced single quotes with double quotes

--- modules/exer2/manifests/schedule.pp
+++ modules/exer2/manifests/schedule.pp
@@ -5,14 +5,14 @@ class exer2::schedule (
 ) inherits exer2 {
 
   cron { 'my_memory_check_root':
-    command => '/home/monitor/src/my_memory_check -e $email -w $warning -c $critical',
+    command => "/home/monitor/src/my_memory_check -e $email -w $warning -c $critical",
     user    => 'root',
     hour    => 0,
     minute  => 10,
   }
 
   cron { 'my_memory_check_monitor':
-    command => '/home/monitor/src/my_memory_check -e $email -w $warning -c $critical',
+    command => "/home/monitor/src/my_memory_check -e $email -w $warning -c $critical",
     user    => 'monitor',
     hour    => 0,
     minute  => 10,
